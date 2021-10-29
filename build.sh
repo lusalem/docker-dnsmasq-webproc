22b31111cb4c0c83e8cc74edb06364b0fe873d66
Salem <39432507+lusalem@users.noreply.github.com>
2021-10-30 04:37:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64 -t marcovolpato00/dnsmasq-webproc:latest . --push
+docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64 -t salemlu/dnsmasq:latest . --push
 # docker build -t marcovolpato00/dnsmasq-webproc:latest .
